26186dc68477033ce76e645c9601b26f56bf0dd0
Wayne Chen <wayne12190@gmail.com>
2020-08-21 16:10:42 -0700

    c

--- routes/companies.js
+++ routes/companies.js
@@ -7,7 +7,7 @@ const express = require("express");
 
 const { BadRequestError } = require("../expressError");
 const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
-const Company = require("../models/Company");
+const Company = require("../models/company");
 
 const companyNewSchema = require("../schemas/companyNew.json");
 const companyUpdateSchema = require("../schemas/companyUpdate.json");
